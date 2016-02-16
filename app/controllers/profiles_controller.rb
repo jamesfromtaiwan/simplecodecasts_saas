bf674c76295a9e4bf6b54f1a74be02db7367bd0a
James Lo <lo.jimmy1994@gmail.com>
2016-02-16 07:29:41 +0000

    Added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,11 @@
 class ProfilesController < ApplicationController
    
    def new
+      # form where user can fill out their own profile
+      @user = User.find(params[:user_id])
+      # to determine who is logged in 
+      @profile = @user.build_profile
+      # generate by nest resource
        
    end
    
